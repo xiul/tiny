20767bee243238923ec6aca554697378809735f7
morenojhon <morenojhon@gmail.com>
2015-04-16 10:23:16 -0430

    creacion del nod For

--- src/ast/NodoFor.java
+++ src/ast/NodoFor.java
@@ -1,5 +1,60 @@
 package ast;
 
 public class NodoFor {
+    private String variable;
+	private NodoBase asig;
+    private NodoBase comprob;
+	private NodoBase paso;
+    private NodoBase cuerpo;
+	
+	public NodoFor(String variable, NodoBase asig, NodoBase comprob,NodoBase paso,NodoBase cuerpo){
+
+        super();
+        this.variable = variable;
+        this.asig = asig;
+        this.comprob = comprob;
+        this.paso = paso;
+        this.cuerpo = cuerpo;
+    }
+	
+	public String getVariable() {
+		return variable;
+	}
+
+    public void setVariable(String variable) {
+		this.variable = variable;
+	}
+
+    public NodoBase getAsig() {
+		return asig;
+	}
+
+    public void setAsig(NodoBase comprob) {
+		this.asig = asig;
+	}
+
+    public NodoBase getComprob() {
+		return comprob;
+	}
+
+    public void setComprob(NodoBase comprob) {
+		this.comprob = comprob;
+	}
+
+    public NodoBase  getPaso() {
+		return paso;
+	}
+
+    public void setPaso(NodoBase  paso) {
+		this.paso=paso;
+	}
+    
+    public NodoBase getCuerpo() {
+		return cuerpo;
+	}
+
+    public void setCuerpo(NodoBase cuerpo) {
+		this.cuerpo = cuerpo;
+	}
 
 }
